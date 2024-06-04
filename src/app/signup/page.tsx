87f762a841dd24781d48d42bdfffbbb54c789794
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-06-04 08:42:34 +0100

    fix: changed login & sign up width

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -12,14 +12,53 @@ import Image from "next/image";
 const page = () => {
   return (
     <>
-      <main className="flex items-center mt-10 sm:mt-16 lg:mt-0">
+      <main className="flex items-center mt-10 sm:mt-16 md:mt-0">
+        <section className="hidden lg:block relative min-h-screen lg:flex-1">
+          <Image
+            src="/images/create.svg"
+            alt="Create your account"
+            width={500}
+            height={500}
+            priority={true}
+            className="absolute w-full object-cover h-full"
+          />
+        </section>
+
         <section className="space-y-8 w-[90%] lg:flex-1 mx-auto lg:px-20">
           <div className="space-y-8">
             <h1 className="text-gray-900 font-semibold text-3xl text-center">
-              Sign in to your account
+              Create your account
             </h1>
 
             <form action="" className="space-y-6">
+              <div className="flex items-center justify-between gap-x-5 md:gap-x-8">
+                <div className="grid w-full items-center gap-1.5">
+                  <Label htmlFor="firstName" className="text-gray-900">
+                    Full Name
+                  </Label>
+                  <Input type="text" id="firstName" placeholder="First Name" />
+                </div>
+
+                <div className="grid w-full items-center gap-1.5">
+                  <Label htmlFor="lastName" className="text-gray-900">
+                    Last Name
+                  </Label>
+                  <Input type="text" id="lastName" placeholder="Last Name" />
+                </div>
+              </div>
+
+              <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor="username" className="text-gray-900">
+                  Username
+                </Label>
+                <Input
+                  type="text"
+                  id="username"
+                  placeholder="Username"
+                  className="w-full"
+                />
+              </div>
+
               <div className="grid w-full items-center gap-1.5">
                 <Label htmlFor="email" className="text-gray-900">
                   Email
@@ -27,20 +66,33 @@ const page = () => {
                 <Input type="email" id="email" placeholder="Email ID" />
               </div>
 
-              <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="password" className="text-gray-900">
-                  Password
-                </Label>
-                <Input
-                  type="password"
-                  id="password"
-                  placeholder="Enter Password"
-                />
+              <div className="flex items-center justify-between gap-x-5 md:gap-x-8">
+                <div className="grid w-full items-center gap-1.5">
+                  <Label htmlFor="password" className="text-gray-900">
+                    Password
+                  </Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    placeholder="Enter Password"
+                  />
+                </div>
+
+                <div className="grid w-full items-center gap-1.5">
+                  <Label htmlFor="confirmPassword" className="text-gray-900">
+                    Confirm password
+                  </Label>
+                  <Input
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="Confirm Password"
+                  />
+                </div>
               </div>
 
               <Button type="button" className="bg-[#020617]">
                 <div className="flex items-center gap-x-1">
-                  <p>Sign in</p>
+                  <p>Create account</p>
                   <ArrowNarrowRight />
                 </div>
               </Button>
@@ -81,17 +133,6 @@ const page = () => {
               </Button>
             </div>
           </div>
-        </section>
-
-        <section className="hidden lg:block relative min-h-screen lg:flex-1">
-          <Image
-            src="/images/login.svg"
-            alt="Log in to your account"
-            width={500}
-            height={500}
-            priority={true}
-            className="absolute w-full object-cover h-full"
-          />
         </section>
       </main>
     </>
